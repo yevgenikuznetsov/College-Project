9cf76ca8f82eec6f2c7ee83d91b6bd38adb54b2b
yevgenikuznetsov <49569713+yevgenikuznetsov@users.noreply.github.com>
2019-04-13 17:11:59 +0300

    Create numberGame.h

---
+++ C Programming/Year2-Sem1-HW1/numberGame.h
@@ -0,0 +1,15 @@
+/*
+ * numberGame.h
+ *
+ *  Created on: Nov 17, 2018
+ *      Author: yevgeni
+ */
+
+#ifndef NUMBERGAME_H_
+#define NUMBERGAME_H_
+
+void createMatToForTheGame();
+void choice(int* mat, int rows, int cols);
+
+
+#endif /* NUMBERGAME_H_ */
