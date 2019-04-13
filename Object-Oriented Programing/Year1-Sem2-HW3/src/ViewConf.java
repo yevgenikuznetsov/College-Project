6f3dd21e4de9a1294a43674099d8fdeb19aa56e8
Yevgeni <yevgenikuz@gmail.com>
2019-04-13 18:33:45 +0300

    Create ViewConf.java

---
+++ Object-Oriented Programing/Year1-Sem2-HW3/src/ViewConf.java
@@ -0,0 +1,14 @@
+/**
+ * Yevgeni kuznetsov id :319498580
+ */
+public class ViewConf {
+	public final static int width = 930;
+	public final static int Length = 300;
+	public final static int durationTime = 10;
+	public final static int space = 20;
+	public final static int widthsecond = 500;
+	public final static int Lengthsecond = 400;
+	public final static int Vgap = 15;
+	public final static int Hgap = 15;
+	public final static int size = 15;
+}
