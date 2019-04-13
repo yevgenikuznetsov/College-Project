6a5b74583ad1c4722b80389e4a388270f8cc36cb
yevgenikuznetsov <49569713+yevgenikuznetsov@users.noreply.github.com>
2019-04-13 17:12:33 +0300

    Create pictureManipulation.h

---
+++ C Programming/Year2-Sem1-HW1/pictureManipulation.h
@@ -0,0 +1,15 @@
+/*
+ * pictureManipulation.h
+ *
+ *  Created on: Nov 16, 2018
+ *      Author: yevgeni
+ */
+
+#ifndef PICTUREMANIPULATION_H_
+#define PICTUREMANIPULATION_H_
+
+void introduction ();
+void startPicture();
+void choice(int* mat, int rows, int cols);
+
+#endif /* PICTUREMANIPULATION_H_ */
