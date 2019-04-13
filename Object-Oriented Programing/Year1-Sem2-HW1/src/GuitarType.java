1d9474b94f19dba769f46e18484ece3cf4c275b7
Yevgeni <yevgenikuz@gmail.com>
2019-04-13 17:55:27 +0300

    Create GuitarType.java

---
+++ Object-Oriented Programing/Year1-Sem2-HW1/src/GuitarType.java
@@ -0,0 +1,6 @@
+/* guitar type const */
+public class GuitarType {
+	public final static String Electric = "Electric";
+	public final static String Acoustic = "Acoustic";
+	public final static String Classic = "Classic";
+}
