0a26b573e92770ea5223946fca7f41ed4e761230
Yevgeni <yevgenikuz@gmail.com>
2019-04-13 17:55:06 +0300

    Create WindInstrumentMaterial.java

---
+++ Object-Oriented Programing/Year1-Sem2-HW1/src/WindInstrumentMaterial.java
@@ -0,0 +1,6 @@
+/* material const */
+public class WindInstrumentMaterial {
+	public final static String Wood = "Wood";
+	public final static String Metal = "Metal";
+	public final static String Plastic = "Plastic";
+}
