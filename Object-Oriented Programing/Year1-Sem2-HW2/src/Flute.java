8ff5023d9a76ea27d2b098e6f2be90085e0e0ec1
Yevgeni <yevgenikuz@gmail.com>
2019-04-13 17:58:51 +0300

    Create Flute.java

---
+++ Object-Oriented Programing/Year1-Sem2-HW2/src/Flute.java
@@ -0,0 +1,57 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+//name : yevgeni kuznetsov
+//id: 319498580
+
+public class Flute extends WindInstrument {
+	public static final String[] FLUET_TYPE = { "Flute", "Recorder", "Bass" };
+
+	private String fluteType;
+
+	public Flute(String brand, Number price, String material, String fluteType) {
+		super(brand, price, material);
+		setFluteType(fluteType);
+	}
+
+	public Flute(Scanner scanner) {
+		super(scanner);
+		setFluteType(scanner.nextLine());
+	}
+
+	public String getFluteType() {
+		return fluteType;
+	}
+
+	public void setFluteType(String fluteType) {
+		if (!isFluteType(fluteType))
+			throw new InputMismatchException("Invalid flute type: " + fluteType);
+
+		this.fluteType = fluteType;
+	}
+
+	private boolean isFluteType(String input) {
+		return isValidType(FLUET_TYPE, input);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (!super.equals(o))
+			return false;
+
+		if (!(o instanceof Flute))
+			return false;
+
+		return getFluteType().equals(((Flute) o).getFluteType());
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + String.format(" Type: %7s", getFluteType().toString());
+	}
+
+	@Override
+	protected Flute clone() throws CloneNotSupportedException {
+		return new Flute(getBrand(), getPrice(), getMaterial(), getFluteType());
+	}
+}
