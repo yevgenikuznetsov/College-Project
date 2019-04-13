0a3734433ea860b41aff498e5d724f22f6ef932a
Yevgeni <yevgenikuz@gmail.com>
2019-04-13 17:55:23 +0300

    Create FlutesType.java

---
+++ Object-Oriented Programing/Year1-Sem2-HW1/src/FlutesType.java
@@ -0,0 +1,6 @@
+/* flutes type const */
+public class FlutesType {
+	public final static String bass = "Bass";
+	public final static String Recorder = "Recorder";
+	public final static String Flute = "Flute";
+}
