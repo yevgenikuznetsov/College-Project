713f838acff5c613547fb50599bc1dbe01c1c7e8
Yevgeni <yevgenikuz@gmail.com>
2019-05-15 23:15:06 +0300

    Create check.h

---
+++ Operation Systems/check.h
@@ -0,0 +1,18 @@
+/*
+ * check.h
+ *
+ *  Created on: May 11, 2019
+ *      Author: yevgeni
+ */
+
+#ifndef CHECK_H_
+#define CHECK_H_
+
+int** convert_to_matrix_child(int array[81]);
+void initialArray_child(int* array);
+int CheckCol_child(int** Matrix);
+int CheckRow_child(int** Matrix);
+int CheckBox_child(int** Matrix);
+
+
+#endif /* CHECK_H_ */
